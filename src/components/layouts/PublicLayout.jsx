9e624a7255c289e34684abaea79662fc3919da7f
Leandro Retamar <lea.retamar@gmail.com>
2025-09-10 21:14:19 -0300

    acoplamiento api home y productos

--- src/components/layouts/PublicLayout.jsx
+++ src/components/layouts/PublicLayout.jsx
@@ -3,25 +3,28 @@ import { Toaster } from "sonner";
 import { Header } from "@/components/common/Header";
 import { Footer } from "@/components/common/Footer";
 import { CartSidebar } from "@/components/common/CartSidebar";
+import { PublicDataProvider } from "@/context/PublicDataProvider";
 
 const PublicLayout = () => {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1">
-        <Outlet />
-      </main>
-      <Footer />
-      <CartSidebar />
-      <Toaster 
-        position="bottom-right"
-        toastOptions={{
-          style: { background: '#f7a82a', color: 'white' },
-          className: 'empanada-toast',
-        }}
-      />
-    </div>
-  )
+    return (
+        <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-1">
+                <PublicDataProvider>
+                    <Outlet />
+                </PublicDataProvider>
+            </main>
+            <Footer />
+            <CartSidebar />
+            <Toaster
+                position="bottom-right"
+                toastOptions={{
+                    style: { background: '#f7a82a', color: 'white' },
+                    className: 'empanada-toast',
+                }}
+            />
+        </div>
+    )
 }
 
 export default PublicLayout
--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 const API_BASEURL = ""
 
 const API_AUTHURL = "";
-const API_CATALOGURL = "https://nonino-catalog.fly.dev";
+const API_CATALOGURL = "https://nonino-catalog.fly.dev/public/catalog/stores/1/menu";
 const API_INVENTORYURL = "";
 const API_STOREURL = "";
 const API_ORDERURL = "/public/catalog/stores/1/menu";

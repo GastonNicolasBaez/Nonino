8cedc70620b442a3bc91fac166b67d7815a47b9d
Leandro Retamar <lea.retamar@gmail.com>
2025-09-30 20:51:33 -0300

    ch2

--- src/config/apiEndpoints.js
+++ src/config/apiEndpoints.js
@@ -3,4 +3,5 @@ export const ENDPOINTS = {
     "stores": 'https://nonino-store.fly.dev',
     "auth": 'https://nonino-auth.fly.dev',
     "inventory": 'https://nonino-inventory.fly.dev',
+    "orders": 'https://nonino-orders.fly.dev',
 };
